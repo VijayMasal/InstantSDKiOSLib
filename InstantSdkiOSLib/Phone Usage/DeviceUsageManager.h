70595f6e020a48db1a77260dc9867803d02552c2
vijay <vdmasal@gmail.com>
2017-11-07 19:26:05 +0530

    update pod

--- InstantSdkiOSLib/Phone Usage/DeviceUsageManager.h
+++ InstantSdkiOSLib/Phone Usage/DeviceUsageManager.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <UIDevice+PasscodeStatus.h>
+#import "UIDevice+PasscodeStatus.h"
 #import "LocationNameAndTime.h"
 
 @protocol  DeviceUsageDelegate ;
